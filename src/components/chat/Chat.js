1df383b52bdbf1d94b4a5fa14c66e449de007f11
Bloo Light <cloud.star@outlook.com>
2021-01-28 19:03:54 +0100

    implemented the frontend design

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,49 +1,150 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Button from '@material-ui/core/Button';
+import { Button, Box, Avatar, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { initChat, sendMessage } from '../../store/reducers/chat';
 
-const chatWrapper = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  padding: '20px',
-  overflowY: 'auto',
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding: '20px',
+    overflowY: 'auto',
+    backgroundColor: '#f1f1f1'
+  },
+  form: {
+    maxWidth: '760px',
+    margin: '0 auto',
+    boxShadow: '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)'
+  },
+  layout: {
+    height: '500px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    // overflowY: 'scroll',
+    backgroundColor: '#ffffff',
+    padding: '24px',
+    boxShadow: 'inset 0px -2px 3px #efefef'
+  },
+  actionWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '16px 24px',
+    backgroundColor: '#fdfdfd'
+  },
+  sendButton: {
+    width: '120px',
+    marginLeft: '20px'
+  },
+  bubbleBot: {
+    display: 'flex',
+    alignItems: 'end',
+    margin: '5px 0'
+  },
+  bubbleUser: {
+    display: 'flex',
+    alignItems: 'end',
+    flexDirection: 'row-reverse',
+    margin: '5px 0'
+  },
+  messageBot: {
+    background: '#f2f6f9',
+    margin: '0',
+    padding: '11px',
+    marginLeft: '10px',
+    borderRadius: '10px',
+    maxWidth: '480px',
+    textAlign: 'left'
+  },
+  messageUser: {
+    background: '#daf4fd',
+    margin: '0',
+    padding: '11px',
+    marginRight: '10px',
+    borderRadius: '10px',
+    maxWidth: '480px',
+    textAlign: 'left'
+  }
+}));
 
 const ChatBubble = (props) => {
   const { history } = props;
+  const classes = useStyles();
+  const isBot = history.from === 'bot';
+
   return (
-    <div>
-      <p>{history.from} : {history.message}</p>
+    <div className={isBot ? `${classes.bubbleBot}` : `${classes.bubbleUser}`}>
+      <Avatar alt={history.from} src={isBot ? '/static/images/avatar/1.jpg'  : '/static/images/avatar/2.jpg'}/>
+      <p className={isBot ? `${classes.messageBot}` : `${classes.messageUser}`}>{history.message}</p>
     </div>
   )
 };
 
 export const Chat = () => {
   const dispatch = useDispatch();
+  const [ message, setMessage ] = useState('');
+  const [ available, setAvailable ] = useState(false);
   const chatHistory = useSelector((state) => state.chat.chatHistory);
+  const isInitialized = useSelector((state) => state.chat.isInitialized);
+  const classes = useStyles();
+
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+    if (event.target.value !== '') {
+      setAvailable(true);
+    } else {
+      setAvailable(false);
+    }
+  };
+
   const handleMessageSend = () => {
-    console.log('chat history ::: ', chatHistory);
-    const message = 'Test message';
+    if (message === '')
+      return;
+
     dispatch(sendMessage(message));
+    setMessage('');
+    setAvailable(false);
+  };
+
+  const onKeyDown = (event) => {
+    if(event.keyCode === 13) {
+      handleMessageSend();
+    }
   };
 
   useEffect(() => {
     dispatch(initChat());
-  });
+  }, []);
 
   return (
-    <div style={chatWrapper}>
+    <div className={classes.root}>
       <h2>Chat Room</h2>
-      <div>
-        {chatHistory.map((history, index) => {
-          return <ChatBubble key={index} history={history}/>
-        })}
+      <div className={classes.form}>
+        <div className={classes.layout}>
+          {chatHistory.map((history, index) => {
+            return <ChatBubble key={index} history={history}/>
+          })}
+        </div>
+        <div className={classes.actionWrapper}>
+          <TextField
+              id="message"
+              placeholder="Type message here..."
+              fullWidth
+              value={message}
+              onKeyDown={onKeyDown}
+              onChange={handleInputChange}/>
+          <Button
+              className={classes.sendButton}
+              variant="contained"
+              color="primary"
+              disabled={!isInitialized || !available}
+              onClick={handleMessageSend}>Send</Button>
+        </div>
       </div>
-      <Button onClick={handleMessageSend}>Click me</Button>
     </div>
   );
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,9 +20,6 @@ export const Header = () => {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar variant="dense">
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" color="inherit">
               Cognigy Chat
             </Typography>
